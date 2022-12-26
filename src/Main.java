654033f1ee22742185bfe40e3eaa0c49a5fc13ef
Alexey <ReetzCraft@gmail.com>
2022-12-26 21:45:13 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Car lada = new Car("Lada", "Granta", 1.7, "yellow", 2015, "Russia");
+        Car lada1 = new Car("Lada", null, -5, null, -5, "Russia");
+        System.out.println(lada);
+        System.out.println(lada1);
+        Car audi = new Car("Audi", "A8", 3.0, "black", 2020, "Germany");
+        System.out.println(audi);
     }
 }
